9d91ba7868961eb39ac3a13f0b472d9d3dd5fb47
2knn10 <109735963+2knn10@users.noreply.github.com>
2022-10-25 15:26:02 +0300

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,15 @@
+operator =  input("Please Enter The operator : ")
+num1 = int(input("Enter Number : " ))
+num2 = int(input("Enter Two Number : " ))
+    
+if operator == "+":
+        print(num1 + num2 )
+elif operator == "-":
+        print(num1 - num2 )
+elif operator == "/":
+        print(num1 / num2 )
+elif operator == "*":
+        print(num1 * num2 )
+else:
+    print("Error")
+    
